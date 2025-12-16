0c2b165f81be4d88566835d6bcae8ff24c334eb5
eloranta <hannu.eloranta@gmail.com>
2025-12-16 12:15:11 +0200

    remove add point button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -32,15 +32,13 @@ MainWindow::MainWindow(QWidget *parent)
     pointCounter_ = canvas_->pointCount() + 1;
     auto *controls = new QHBoxLayout();
     controls->setSpacing(8);
-    auto *addPointBtn = new QPushButton("Add Point", central);
-    auto *addLineBtn = new QPushButton("Add Line", central);
-    auto *extendLineBtn = new QPushButton("Extend Line", central);
-    auto *addCircleBtn = new QPushButton("Add Circle", central);
+    auto *addLineBtn = new QPushButton("Connect", central);
+    auto *extendLineBtn = new QPushButton("Extend", central);
+    auto *addCircleBtn = new QPushButton("Circle", central);
     auto *intersectBtn = new QPushButton("Normal", central);
     auto *intersectionsBtn = new QPushButton("Intersect", central);
     auto *deleteBtn = new QPushButton("Delete", central);
     auto *deleteAllBtn = new QPushButton("Delete All", central);
-    controls->addWidget(addPointBtn);
     controls->addWidget(addLineBtn);
     controls->addWidget(extendLineBtn);
     controls->addWidget(addCircleBtn);
@@ -51,7 +49,6 @@ MainWindow::MainWindow(QWidget *parent)
     controls->addStretch(1);
     layout->addLayout(controls);
 
-    connect(addPointBtn, &QPushButton::clicked, this, &MainWindow::showAddPointDialog);
     connect(addLineBtn, &QPushButton::clicked, this, &MainWindow::onAddLineClicked);
     connect(extendLineBtn, &QPushButton::clicked, this, &MainWindow::onExtendLineClicked);
     connect(addCircleBtn, &QPushButton::clicked, this, &MainWindow::onAddCircleClicked);
@@ -61,53 +58,6 @@ MainWindow::MainWindow(QWidget *parent)
     connect(deleteAllBtn, &QPushButton::clicked, this, &MainWindow::onDeleteAllClicked);
 
     setCentralWidget(central);
-}
-
-void MainWindow::showAddPointDialog() {
-    QDialog dialog(this);
-    dialog.setWindowTitle("Add Point");
-
-    auto *form = new QFormLayout(&dialog);
-    auto *x = new QDoubleSpinBox(&dialog);
-    auto *y = new QDoubleSpinBox(&dialog);
-    auto *labelEdit = new QLineEdit(&dialog);
-
-    const double min = -1000.0;
-    const double max = 1000.0;
-    for (auto *spin : {x, y}) {
-        spin->setRange(min, max);
-        spin->setDecimals(3);
-        spin->setSingleStep(0.1);
-    }
-
-    x->setValue(0.0);
-    y->setValue(0.0);
-    labelEdit->setText(QString("P%1").arg(pointCounter_));
-    labelEdit->setMaxLength(16);
-
-    form->addRow("X (-5..5):", x);
-    form->addRow("Y (-5..5):", y);
-    form->addRow("Label:", labelEdit);
-
-    auto *buttons = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, &dialog);
-    form->addWidget(buttons);
-    connect(buttons, &QDialogButtonBox::accepted, &dialog, &QDialog::accept);
-    connect(buttons, &QDialogButtonBox::rejected, &dialog, &QDialog::reject);
-
-    if (dialog.exec() == QDialog::Accepted) {
-        QString label = labelEdit->text().trimmed();
-        if (label.isEmpty()) {
-            label = QString("P%1").arg(pointCounter_);
-        }
-        QPointF pt(x->value(), y->value());
-        if (canvas_->hasPoint(pt)) {
-            QMessageBox::information(this, "Point Exists", "A point at these coordinates already exists.");
-            return;
-        }
-        if (canvas_->addPoint(pt, label)) {
-            ++pointCounter_;
-        }
-    }
 }
 
 void MainWindow::onAddLineClicked() {

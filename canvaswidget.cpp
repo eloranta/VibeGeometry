59983bfd228aa75d8ed697ff56c26edbfc03f884
eloranta <hannu.eloranta@gmail.com>
2025-12-16 14:33:00 +0200

    remove: // Axes/ticks intentionally hidden for now

--- canvaswidget.cpp
+++ canvaswidget.cpp
@@ -588,25 +588,7 @@ void CanvasWidget::paintEvent(QPaintEvent *event) {
         return QPointF(origin.x() + x * scale, origin.y() - y * scale);
     };
 
-    QPen axisPen(Qt::black, 2);
-    painter.setPen(axisPen);
-    painter.drawLine(map(-5, 0), map(5, 0));  // X axis
-    painter.drawLine(map(0, -5), map(0, 5));  // Y axis
-
-    QPen tickPen(Qt::black, 1);
-    painter.setPen(tickPen);
-    const double tickSize = 6.0;
-    for (int i = -5; i <= 5; ++i) {
-        if (i == 0) continue;  // skip origin tick overlap
-        // X ticks
-        QPointF p1 = map(i, -tickSize / scale / 2.0);
-        QPointF p2 = map(i, tickSize / scale / 2.0);
-        painter.drawLine(p1, p2);
-        // Y ticks
-        QPointF p3 = map(-tickSize / scale / 2.0, i);
-        QPointF p4 = map(tickSize / scale / 2.0, i);
-        painter.drawLine(p3, p4);
-    }
+    // Axes/ticks intentionally hidden for now
 
     painter.setPen(QPen(Qt::blue, 2));
     for (int i = 0; i < lines.size(); ++i) {
